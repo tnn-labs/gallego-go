c253cbd2ec9bc1d48579c5059c9074855ac60eb2
theandersonn <theandersonn@gmail.com>
2022-06-08 18:27:03 -0300

    2-10: Arrays internos

--- 2-fundamentos/10-arrays-e-slices/arrays-e-slices.go
+++ 2-fundamentos/10-arrays-e-slices/arrays-e-slices.go
@@ -32,4 +32,23 @@ func main() {
 
 	coresEscolhidas := cores[0:2]
 	fmt.Println(coresEscolhidas)
+
+	// ARRAYS INTERNOS
+	// criando um array de 11 posições e pegando as 10 primeiras
+	sliceExemplo := make([]float32, 10, 11)
+	fmt.Println(sliceExemplo)
+	fmt.Println(len(sliceExemplo)) // lenght
+	fmt.Println(cap(sliceExemplo)) // capacidade
+
+	sliceExemplo = append(sliceExemplo, 5)
+	fmt.Println(sliceExemplo)
+	sliceExemplo = append(sliceExemplo)
+	fmt.Println(len(sliceExemplo)) // lenght
+	fmt.Println(cap(sliceExemplo)) // capacidade
+
+	sliceExemplo = append(sliceExemplo, 6)
+	fmt.Println(sliceExemplo)
+	sliceExemplo = append(sliceExemplo)
+	fmt.Println(len(sliceExemplo)) // lenght
+	fmt.Println(cap(sliceExemplo)) // capacidade
 }
